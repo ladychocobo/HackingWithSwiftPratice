8554c579d3f6eb689b00a756f2df44c5c123eff6
Jamie Chen <lydiaxi.wow@gmail.com>
2017-05-31 21:46:05 +0800

    Project 2 added

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Jamie Chen on 5/31/17.
+//  Copyright © 2017 Jamie Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
